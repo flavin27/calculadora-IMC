8ff27b3cff48776aea987b02e95141c7a7fd698f
Flavio <77132531+flaviodev27@users.noreply.github.com>
2023-01-22 18:22:33 -0300

    script feito

---
+++ function.js
@@ -0,0 +1,18 @@
+const form = document.querySelector('#formulario');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const peso = Number((e.target.querySelector('#peso')).value);
+    const altura = Number((e.target.querySelector('#altura')).value);
+    const resultado = window.document.getElementById('resultado')
+    function calcularIMC(peso, altura) {
+         const IMC = (peso / (altura * altura)).toFixed(2)
+        return IMC
+    }
+    const imc = calcularIMC(peso, altura)
+    resultado.innerHTML += `<p> Seu IMC vale ${imc}</p>`
+})
+
+
+
+
