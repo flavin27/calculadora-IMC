2780b79e96e3fd3510b8abe21506018f89b76103
Flavio <77132531+flaviodev27@users.noreply.github.com>
2023-01-23 18:49:26 -0300

    criei as codicoes

--- function.js
+++ function.js
@@ -17,7 +17,25 @@ form.addEventListener('submit', function (e) {
     } else {
         resultado.innerHTML = `<p> Seu IMC vale ${imc}</p>`
     }
-    
+    if (imc < 18.5) {
+        resultado.innerHTML += `<p id='cor'> Voce está abaixo do peso!</p>`
+        document.getElementById("cor").style.color = "red"
+    } else if (imc >= 18.5 && imc <= 24.9) {
+        resultado.innerHTML += `<p> Seu peso está normal.`
+        document.getElementById('cor'),style.color = 'green'
+    } else if (imc >= 25 && imc <= 29.9) {
+        resultado.innerHTML += `<p id='cor'> Voce esta com excesso de peso!`
+        document.getElementById('cor').style.color = 'red'
+    } else if (imc >= 30 && imc <= 34.9) {
+        resultado.innerHTML += `<p id='cor'> Voce esta com obesidade tipo 1!`
+        document.getElementById('cor').style.color = 'red'
+    } else if (imc >= 35 && imc <= 39.9 ) {
+        resultado.innerHTML += `<p id='cor'> Voce esta com obesidade tipo 2!`
+        document.getElementById('cor').style.color = 'red'
+    } else if (imc >= 40) {
+        resultado.innerHTML += `<p id='cor'> Voce esta com obesidade tipo 3!`
+        document.getElementById('cor').style.color = 'red'
+    }
 })
 
 
