cd3f6b904c991d0016de9064604c8084b2b09148
Flavio <77132531+flaviodev27@users.noreply.github.com>
2023-01-23 18:31:40 -0300

    adicionei validacao

--- function.js
+++ function.js
@@ -6,12 +6,20 @@ form.addEventListener('submit', function (e) {
     const altura = Number((e.target.querySelector('#altura')).value);
     const resultado = window.document.getElementById('resultado')
     function calcularIMC(peso, altura) {
-         const IMC = (peso / (altura * altura)).toFixed(2)
+        const IMC = (peso / (altura * altura)).toFixed(2)
         return IMC
     }
     const imc = calcularIMC(peso, altura)
-    resultado.innerHTML += `<p> Seu IMC vale ${imc}</p>`
+    if (peso == undefined || peso == 0) {
+        window.alert('Digite um peso válido')
+    } else if (altura == undefined || altura == 0) {
+        window.alert('Digite uma altura valida')
+    } else {
+        resultado.innerHTML = `<p> Seu IMC vale ${imc}</p>`
+    }
+    
 })
+
 
 
 
